c2d60b1dc07b7fa12e517a9a0ba9f06bdf2fb63b
godchronos <72Jf0#EfH*59>
2017-10-18 05:12:10 +0300

    add

--- BtcRelaxTelegramBot.js
+++ BtcRelaxTelegramBot.js
@@ -18,25 +18,35 @@ function prepareUpdateRequest(vEntry)
   }
   else
   {
-    cEntry.set("ServerRequest",mU+'getUpdates');
+ cEntry.set("ServerRequest",mU+'getUpdates');
   };    
-}
+};
+
 
 function getUpdateInfo(vEntry)
 {
-    var vRequest = vEntry.field("ServerRequest");
-    var result=http().get(vRequest);
-    if(result.code===200) {
-                var json=JSON.parse(result.body);
-                var state =json.ok;
-                if (state===true)
-                {
-                    vEntry.set("ServerResponse",JSON.stringify(json));            
-                    
-                }
-                else
-                {
-                    message('Error while get updates!');
-                };
+var vRequest = vEntry.field("ServerRequest");
+var result=http().get(vRequest);
+if(result.code===200) {
+   var json=JSON.parse(result.body);
+   var state =json.ok;
+   if (state===true)
+   { 
+vEntry.set("ServerResponse",JSON.stringify(json));
+   var vUList=json.result;
+   for (var UInfo in vUList)
+    {
+       var uI=vUList[UInfo];
+       var vLast=vEntry.field("UpdateId");
+       if (vLast!==uI.update_id)
+{
+message(uI.update_id);
+};
     };
-}
+   };
+  };
+};
+
+var vC=entry();
+prepareUpdateRequest(vC);
+getUpdateInfo(vC);
