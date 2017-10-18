4ff88a14a677b4eaa80ba0e8be983f0fe9819bbf
godchronos <72Jf0#EfH*59>
2017-10-18 05:44:26 +0300

    proceessing update

--- BtcRelaxTelegramBot.js
+++ BtcRelaxTelegramBot.js
@@ -14,7 +14,7 @@ function prepareUpdateRequest(vEntry)
   var vO=cEntry.field("UpdateId");
   if (vO>0)
   {
-    cEntry.set("ServerRequest",mU+'getUpdates&offset='+vO);
+    cEntry.set("ServerRequest",mU+'getUpdates?offset='+vO);
   }
   else
   {
@@ -29,23 +29,30 @@ var vRequest = vEntry.field("ServerRequest");
 var result=http().get(vRequest);
 if(result.code===200) {
    var json=JSON.parse(result.body);
+   vEntry.set("ServerResponse",JSON.stringify(json));
    var state =json.ok;
    if (state===true)
    { 
-vEntry.set("ServerResponse",JSON.stringify(json));
    var vUList=json.result;
    for (var UInfo in vUList)
     {
        var uI=vUList[UInfo];
        var vLast=vEntry.field("UpdateId");
        if (vLast!==uI.update_id)
-{
-message(uI.update_id);
-};
+       {
+       processUpdateInfo(vEntry,uI);
+       };
     };
    };
   };
 };
+
+function processUpdateInfo(vEntry, vUpdateInfo)
+{
+    var vUId = vUpdateInfo.update_id;
+    vEntry.set("UpdateId",vUId);
+};
+
 
 var vC=entry();
 prepareUpdateRequest(vC);
