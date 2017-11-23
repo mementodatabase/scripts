9e8911af9c50144295656f0d1636689abba708d5
godchronos <godchronos@users.noreply.github.com>
2017-11-23 11:58:05 +0000

    Update bgValidator.js

--- bgValidator.js
+++ bgValidator.js
@@ -12,8 +12,7 @@ BitGanjValidator.prototype.validate = function() {
   var vAdvertiseTitle;
   var vTotalPrice = 0;
   var vBookmarkId = this.cEntry.field("BookmarkId");
-  if (vBookmarkId>0)
-  {
+
   for (var i2 = 0; i2 < curInBox.length; i2++) {
     var linkedEntry = curInBox[i2];
     if (i2 === 0) {
@@ -26,19 +25,19 @@ BitGanjValidator.prototype.validate = function() {
 
   this.cEntry.set("FrontTitle", vAdvertiseTitle);
   var loc = this.cEntry.field("Loc");
-  if (loc!==null)
-  {
-       var nLat = Math.round(loc.lat * 1000000) / 1000000;
-       var nLng = Math.round(loc.lng * 1000000) / 1000000;
-       this.cEntry.set("TotalPrice", vTotalPrice);
-       this.cEntry.set("Latitude", nLat);
-       this.cEntry.set("Longitude", nLng);
+     if (loc!==null)
+		{
+			var nLat = Math.round(loc.lat * 1000000) / 1000000;
+			var nLng = Math.round(loc.lng * 1000000) / 1000000;
+			this.cEntry.set("TotalPrice", vTotalPrice);
+			this.cEntry.set("Latitude", nLat);
+			this.cEntry.set("Longitude", nLng);
 
-       var urlToPic =  this.cEntry.field("PublicURL");
-       if (urlToPic === '') {
-             this.msg = "Отсутствует ссылка на фотографии";
-             this.isValid =false;
-      } else {
+			var urlToPic =  this.cEntry.field("PublicURL");
+			if (urlToPic === '') {
+				this.msg = "Отсутствует ссылка на фотографии";
+				this.isValid =false;
+		} else {
                 var nCmd = null;
                 var cReg =  this.cEntry.field("Region");
                 var vDescr =  this.cEntry.field("PointDescription");
@@ -47,26 +46,24 @@ BitGanjValidator.prototype.validate = function() {
                 if (vRegCounts > 0) {
                 var regObj = cReg[0];
                 vRegion = this.getRegionPath(regObj);
-                      if (vRegion !== null) {
-                          nCmd = '{"type":"AddPoint","inf":{"lat":"' + nLat + '","lng":"' + nLng + '","link":"' + urlToPic + '","advName":"' + vAdvertiseTitle + '","price":"' + vTotalPrice + '","region":"' + vRegion + '","descr":"' + vDescr + '"}}';
-                         this.cEntry.set("ServerRequest", nCmd);
-                         this.msg = '';
-                         this.isValid =true;
-                    } else {
+                if (vRegion !== '') {
+                     nCmd = '{"type":"AddPoint","inf":{"lat":"' + nLat + '","lng":"' + nLng + '","link":"' + urlToPic + '","advName":"' + vAdvertiseTitle + '","price":"' + vTotalPrice + '","region":"' + vRegion + '","descr":"' + vDescr + '"}}';
+                     this.cEntry.set("ServerRequest", nCmd);
+                     this.msg = '';
+                     this.isValid =true;
+					} else {
                       this.msg = "Ошибка определения региона!";
                       this.isValid =false;
-                           };
-                }; };
-  } else
-  {
-     this.msg = "Некорректно заданы координаты!";
-     this.isValid =false;
-  }; 
+                       };
+                }; 
+            };
+		} else
+		{
+			this.msg = "Некорректно заданы координаты!";
+			this.isValid =false;
+		}; 
+		log(this.msg);
 };
-  log(this.msg);
-   
-};
-
 
 
 BitGanjValidator.prototype.getRegionPath = function(entry) {
