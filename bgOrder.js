044fd1788c4a0990668a305c1394140d10a80db2
godchronos <godchronos@users.noreply.github.com>
2017-11-27 12:06:32 +0000

    Update bgOrder.js

--- bgOrder.js
+++ bgOrder.js
@@ -1,5 +1,21 @@
 /* global http, moment */
 function BitGanjOrders(v_lib, v_gate) {
   this.gate = v_gate;
-  this.lib = v_lib;
+  this.clib = v_lib;
  }
+
+BitGanjOrders.prototype.getOrder = function(vEntry) {
+  var res=false;
+  var vOrderId=vEntry.field("OrderId");
+  var vFlib=this.clib;
+  var vCPub=vFlib.findByKey(vOrderId);
+  if (vCPub===null && vOrderId>0)
+  {
+    var newOrder=new Object();
+    newOrder["OrderId"]=vOrderId;
+    var order=vFlib.create(newOrder);
+    order.set("PublicationEntry",vEntry);
+    res=order;
+  } else { res=vCPub; };
+  return res;
+};
