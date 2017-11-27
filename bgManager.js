73abb05a12389e1f51e7c509f8a6d8c0be1fad33
godchronos <godchronos@users.noreply.github.com>
2017-11-27 12:33:53 +0000

    Refresh order by id

--- bgManager.js
+++ bgManager.js
@@ -24,117 +24,6 @@ function BitGanjApi(v_server, v_tokenId, v_tokenKey) {
   };
 }
 
-/*BitGanjApi.prototype.validate = function(pEntry) {
-  var vRes = false;
-  var curInBox = pEntry.field("inBox");
-  var vAdvertiseTitle;
-  var vTotalPrice = 0;
-  var vBookmarkId = pEntry.field("BookmarkId");
-  if (vBookmarkId>0)
-    {
-      pEntry.set("State",'Prepared');
-    };
-  for (var i2 = 0; i2 < curInBox.length; i2++) {
-    var linkedEntry = curInBox[i2];
-    if (i2 === 0) {
-      vAdvertiseTitle = linkedEntry.field("ItemTypeName");
-    } else {
-      vAdvertiseTitle = vAdvertiseTitle + " & " + linkedEntry.field("ItemTypeName");
-    };
-    vTotalPrice = vTotalPrice + linkedEntry.field("DefaultPrice");
-  };
-
-  pEntry.set("FrontTitle", vAdvertiseTitle);
-  var loc = pEntry.field("Loc");
-  var nLat = Math.round(loc.lat * 1000000) / 1000000;
-  var nLng = Math.round(loc.lng * 1000000) / 1000000;
-  pEntry.set("TotalPrice", vTotalPrice);
-  pEntry.set("Latitude", nLat);
-  pEntry.set("Longitude", nLng);
-
-  var urlToPic = pEntry.field("PublicURL");
-  if (urlToPic === '') {
-    vRes = "Url for name:" + AdvertiseTitle + " and total price:" + vTotalPrice + " not found";
-  } else {
-    var nCmd = null;
-    var cReg = pEntry.field("Region");
-    var vDescr = pEntry.field("PointDescription");
-    var vRegCounts = cReg.length;
-    var vRegion = null;
-    if (vRegCounts > 0) {
-      var regObj = cReg[0];
-      vRegion = this.getRegionPath(regObj);
-      if (vRegion !== null) {
-        nCmd = '{"type":"AddPoint","inf":{"lat":"' + nLat + '","lng":"' + nLng + '","link":"' + urlToPic + '","advName":"' + vAdvertiseTitle + '","price":"' + vTotalPrice + '","region":"' + vRegion + '","descr":"' + vDescr + '"}}';
-        pEntry.set("ServerRequest", nCmd);
-        vRes=true;
-      } else {
-        vRes = "Region name error";
-      };
-    };
-  };
-  return vRes;
-}
-
-
-
-BitGanjApi.prototype.getRegionPath = function(entry) {
-  var res;
-  res = entry.field("TitleRu");
-  var parCnt = entry.field("ParentRegion").length;
-  if (parCnt > 0) {
-    res = res + ", " + this.getRegionPath(entry.field("ParentRegion")[0]);
-  };
-  return res;
-};
-
-
-BitGanjApi.prototype.newEntry = function(vEntry) {
-  var res = true;
-  var msg = vEntry.field("ServerRequest");
-  var callUrl = this.server + "/PointsApi.php" + '?tokenId=' + this.tokenId + '&tokenKey=' + this.tokenKey + '&action=';
-  log("Calling URL:" + callUrl + msg);
-  var result = http().get(callUrl + encodeURIComponent(msg));
-  if (result.code == 200) {
-    var json = JSON.parse(result.body);
-    vEntry.set("ServerResponse", JSON.stringify(json));
-    vEntry.set("CallDate",moment().toDate());
-    var rcode = json.code;
-    if (rcode === 0) {
-      var pointId = json.bookmarkId;
-      if (pointId > 0) {
-        vEntry.set("BookmarkId", pointId);
-        var newPub = this.newPublication(vEntry);
-      } else {
-        res = "Entry with URL:" + vEntry.field("PublicURL") + " rejected by server!";
-      };
-    } else {
-      res = json.message;
-    };
-  };
-  return res;
-};
-
-BitGanjApi.prototype.newPublication = function(vEntry) {
-  var res = true;
-  var vPointId=vEntry.field("BookmarkId"); 
-  var vFlib=this.frontLib;
-  var vRpub= vFlib.findByKey(vPointId);
-  if (vRpub===null)
-    {
-      var newPub = new Object();
-      newPub["BookmarkId"] = vPointId;
-      newPub["FrontShopTitle"] = vEntry.field("FrontTitle");
-      newPub["Photos"] = vEntry.field("PublicURL");
-      var pub = vFlib.create(newPub);
-    pub.set("Location", vEntry.field("Loc"));
-    pub.set("Price", vEntry.field("TotalPrice"));
-    vEntry.set("PublicationEntry", pub);
-    pub.set("RegionInfo", vEntry.field("Region"));
-  };
-};
-*/
-
 function syncAll(vServer, vTokenId, vTokenKey) {
   var vCl = lib();
   var vEa = vCl.entries();
@@ -162,7 +51,9 @@ function syncCurrent(vServer, vTokenId, vTokenKey, vEntry) {
          var vOrderId = vEntry.field("OrderId");
          if (vOrderId>0)
          {
+           log("Try to get and refreh order Id:"+vOrderId);
            var vOrder=bga.Orders.getOrder(vPub);
+           var refResult=bga.Orders.refresh(vOrder);
          };
        };
     };
