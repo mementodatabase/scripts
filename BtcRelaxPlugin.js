b882bd652a74388a260542b37dcaf25ad1d65ceb
boomhackerUa <38929916+boomhackerUa@users.noreply.github.com>
2018-08-09 02:08:50 +0300

    Update BtcRelaxPlugin.js

--- BtcRelaxPlugin.js
+++ BtcRelaxPlugin.js
@@ -44,7 +44,7 @@ BtcRelaxApi.prototype.syncEntry = function(vEntry)
 BtcRelaxApi.prototype.newEntry = function(vEntry)
 {
     var msg=vEntry.field("ServerRequest");
-    var callUrl=this.server+'?tokenId='+this.tokenId+'&tokenKey='+this.tokenKey+'&action=';
+    var callUrl=this.server+'?tokenId='+this.tokenId+'&tokenKey='+this.tokenKey+'&request=addBookmark';
     log("Calling URL:"+callUrl+msg);
     var result=http().get(callUrl+encodeURIComponent(msg));  
     if(result.code==200)
