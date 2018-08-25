018789107a0bf0674054917784a791090aa9d82e
boomhackerUa <38929916+boomhackerUa@users.noreply.github.com>
2018-08-25 06:50:12 +0300

    Update bitches.js

--- bitches.js
+++ bitches.js
@@ -6,6 +6,15 @@ function refreshBitches()
          for(var i=0;i<count;i++)
              {
              var cEnt =entries[i];
-             cEnt.set('Website',cEnt.field('galleryUrl'));
+             cEnt.set('Website',cEnt.field('baseUrl'));
              };
+}; 
+
+function refreshBitch(bitch)
+{
+         if (bitch == null)
+         {
+                  bitch = entry();               
+         }
+         bitch.set('Website',cEnt.field('baseUrl'));
 }; 
