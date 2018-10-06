1dae6f7442284d7c3d3d16bbf6ebd490e45ed52a
godjah <godjah@ganj.site>
2018-10-06 10:43:22 -0400

    BtcRelaxApi reworking

--- BtcRelaxPlugin.js
+++ BtcRelaxPlugin.js
@@ -5,3 +5,12 @@ function BtcRelaxApi( v_server ,v_tokenKey )
     this.server = v_server !== null? v_server: 'fastfen.club';
     this.tokenKey = v_tokenKey  !== null? v_tokenKey: null;
 }
+
+BtcRelaxApi.prototype.getVersion = function()
+{
+  result = http().get("https://" + this.server + "/api/GetVer");
+  if (result.code == 200) {
+     var vGetVerResult = JSON.parse(result.body);
+     return vGetVerResult.Core;
+  }
+}
