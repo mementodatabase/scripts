90108913a62d71e6fc8dfd23dfd7ef008405e4b3
godjah <godjah@ganj.site>
2018-10-07 06:03:28 -0400

    get point state for prod

--- BtcRelaxPlugin.js
+++ BtcRelaxPlugin.js
@@ -1,7 +1,5 @@
 /* global http, moment */
-
-function BtcRelaxApi( v_server ,v_tokenKey )
-{
+function BtcRelaxApi( v_server ,v_tokenKey ) {
     this.server = v_server !== null? v_server: 'fastfen.club';
     this.tokenKey = v_tokenKey  !== null? v_tokenKey: null;
 }
