2969c5df924cd4fb9f9aafe1773efe7087e81c58
godchronos <72Jf0#EfH*59>
2018-10-15 23:05:46 +0300

    added teplate

---
+++ Libraries/points_library.js
@@ -0,0 +1,10 @@
+var cLib = lib();
+var entries =cLib.entries();
+var count =entries.length;
+for (i=0;i<count;i++)
+{
+  var cEntry = entries[i];
+  var vAPI = new BtcRelaxApi("shop.bitganj.website");
+  vAPI.getPointState(cEntry);
+}
+
